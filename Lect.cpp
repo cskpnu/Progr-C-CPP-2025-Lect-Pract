9ca60f657e0a7246e1afb38dd2e3ba6f58ffbbb1
VitaliiKN2806 <wivanyuk@kpnu.edu.ua>
2025-02-26 22:44:53 +0200

    l01

---
+++ Lect.cpp
@@ -0,0 +1,6 @@
+#include "iostream"
+
+int main(){
+    std::cout << "dgshjfgsdjh 45645";
+
+}
---
+++ Lection001.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include "cmath"
+#define Pi 3.1415926
+#define SQR(x) (x)*(x)
+using namespace std;
+int main() {
+    cout << "Hello, Group KN1B24!" << std::endl;
+    double a=1,b=2;
+    cout << sin((double)3/2) << std::endl;
+    cout << Pi << std::endl;
+    cout << SQR(5);
+
+    int q=0;
+    cout << "\nq=";
+    std::cin >> q;
+    std::cout << q ;
+    scanf_s("%d",&q);
+    printf_s("q=%d",q);
+    return 0;
+}
