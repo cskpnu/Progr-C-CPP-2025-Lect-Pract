f76d79585d1eee66db9c5b6c398440bc55e0c7ac
wivanyuk <wivanyuk@kpnu.edu.ua>
2025-03-19 10:51:13 +0200

    Update L04-04.cpp

--- L04/L04-04.cpp
+++ L04/L04-04.cpp
@@ -3,15 +3,17 @@
 int main(){
     int nums[10];
     for (int i = 0; i < 10; i++)
-        nums[i] = i * i;
+        nums[i] = (i+1) * (i+1);
     for (int i = 0; i<10; i++){
         std::cout << "nums[" << i << "] = " << nums[i] << std::endl;
     }
 
     int nums1[10] = {0, 1, 4, 9, 16, 25, 36, 49, 64, 81};
     int nums2[10];
-    for (int i = 0; i < 10; i++)
+    for (int i = 0; i < 10; i++){
+        std::cout << "Enter nums2[" << i << "] = ";
         std::cin >> nums2[i];
+    }
     for (int i = 0; i < 10; i++)
         std::cout << "nums2[" << i << "] = " << nums2[i] << std::endl;
     return 0;
